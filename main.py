d5163f1db2e36ffa51ff326132d8296e4e7e7c01
Mu-Ri-H <blooderking321@gmail.com>
2024-02-12 21:17:16 +0700

    initial

---
+++ main.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import streamlit as st
+
+with st.echo("below"):
+    from st_pages import Page, add_page_title, show_pages
+
+    "## Declaring the pages in your app:"
+
+    show_pages(
+        [
+            Page("dashboard.py", "Dashboard", "🏠"),
+            Page("profile.py", "Profile", "🏠"),
+        ]
+    )
+
+    add_page_title()  # Optional method to add title and icon to current page
